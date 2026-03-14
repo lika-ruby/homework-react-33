2acc7b5ec37cfadf54c36e4005c6bf502b9a4cc3
lika-ruby <mary.havrylova.2011@gmail.com>
2026-03-14 12:28:24 +0200

    makes all

---
+++ src/redux/store.js
@@ -0,0 +1,26 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { contactsReducer } from "./contactsSlice";
+import { filterReducer } from "./filterSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+    key: "root",
+    storage,
+    whitelist: ["contacts", "filter"],
+};
+
+const rootReducer = combineReducers({
+    contacts: contactsReducer,
+    filter: filterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({ serializableCheck: false }),
+});
+
+export const persistor = persistStore(store);
